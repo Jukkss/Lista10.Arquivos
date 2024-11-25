e904b225b3a986c06315a71cea895b2208b0a141
Joao Victor Soares Souza <1568660@sga.pucminas.br>
2024-11-25 12:20:38 -0300

    Questão 2 - Finalizada

--- Questão2/Program.cs
+++ Questão2/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿// João Victor Soares Souza - Questão 2  - (Lista10)
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Questão2
 {
@@ -10,6 +12,41 @@ namespace Questão2
     {
         static void Main(string[] args)
         {
+            imprimirPesos();
+            Console.ReadLine();
+        }
+
+        static void imprimirPesos()
+        {
+            StreamReader arqL = new StreamReader("C:\\ATP\\Lista10_ATP\\arquivo-01.txt", Encoding.UTF8);
+            string[] dados;
+            int maiorP = int.MinValue; int menorP = int.MaxValue; int i = 0;
+            double media, somapeso = 0;
+
+            string linha = arqL.ReadLine();
+            int posP = 3;
+            while (linha != null)
+            {
+                dados = linha.Split(';');
+                string peso = dados[posP];
+                if (int.Parse(peso) > maiorP)
+                {
+                    maiorP = int.Parse(peso);
+                }
+                if (int.Parse(peso) < menorP)
+                {
+                    menorP = int.Parse(peso);
+                }
+                i++;
+                somapeso += int.Parse(peso);
+                linha = arqL.ReadLine();
+            }
+            arqL.Close();
+            media = somapeso / i;
+
+            Console.WriteLine($"O maior peso foi: {maiorP}");
+            Console.WriteLine($"O menos peso foi: {menorP}");
+            Console.WriteLine($"A média dos pesos foi: {media}");
         }
     }
 }
