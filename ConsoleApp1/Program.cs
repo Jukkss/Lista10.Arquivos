03b223e157a39e4e348f2d2bb619143109da53b2
Joao Victor Soares Souza <1568660@sga.pucminas.br>
2024-11-25 11:47:34 -0300

    Questão 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿// João Victor Soares Souza - Questão 1 - (Lista10)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            imprimirFelinos();
+        }
+        static void imprimirFelinos()
+        {
+            StreamWriter arqE = new StreamWriter("C:\\ATP\\Lista10_ATP\\cods\\EscritaQ1.txt", false, Encoding.UTF8);
+            StreamReader arqL = new StreamReader("C:\\ATP\\Lista10_ATP\\arquivo-01.txt", Encoding.UTF8);
+            string[] dados;
+            string linha;
+            int i = 1; int c = 0;
+
+            linha = arqL.ReadLine();
+            while (linha != null)
+            { 
+                dados = linha.Split(';');
+                string raca = dados[i];
+                Console.WriteLine($"Raça do felino {c+1}: {raca}");
+                i=+2;
+                c++;
+            }
+            arqL.Close();
+        }
+    }
+}
